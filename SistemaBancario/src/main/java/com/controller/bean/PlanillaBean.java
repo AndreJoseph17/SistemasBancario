645b950166ed933037ebf901e9ac843f70e5fc94
KevinCh19 <kevin.chavezz@ucuenca.ec>
2017-07-20 18:04:43 -0500

    Clases Bean
    Actualizado clases bean en un solo paquete

--- SistemaBancario/src/main/java/com/controller/bean/PlanillaBean.java
+++ SistemaBancario/src/main/java/com/controller/bean/PlanillaBean.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sistemabancario.controller.bean;
+package com.controller.bean;
 
 /**
  *
  * @author EQUIPO
  */
-public class TransferenciaHasCuentaBean {
+public class PlanillaBean {
     
 }
--- SistemaBancario/src/main/java/com/util/HibernateUtil.java
+++ SistemaBancario/src/main/java/com/util/HibernateUtil.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sistemabancario.util;
+package com.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
