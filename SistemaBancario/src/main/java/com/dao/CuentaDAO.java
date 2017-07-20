7091d0c4b668a40ebde46e4ac8c74d139a225768
Andre Joseph Jaramillo <andre.joseph.jaramillo@gmail.com>
2017-07-20 17:49:11 -0500

    Cuenta DAO

--- SistemaBancario/src/main/java/com/dao/CuentaDAO.java
+++ SistemaBancario/src/main/java/com/dao/CuentaDAO.java
@@ -6,11 +6,36 @@
 package com.dao;
 
 import com.model.pojo.Cuenta;
+import com.sistemabancario.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
 
 /**
  *
  * @author andre
  */
 public class CuentaDAO {
+
+    private Cuenta cuenta;
+    private Cuenta newcuenta;
+    private List<Cuenta> DaoAllCuentas;
+    private List<Cuenta> DaoSearchCuentaList;
+
+    public List<Cuenta> AllCuentas() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        try {
+            session.beginTransaction();
+            DaoAllCuentas = session.createCriteria(Cuenta.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return DaoAllCuentas;
+    }
     
 }
